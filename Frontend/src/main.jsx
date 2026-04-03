57029f2e3023e93e0a51c360e0ec0b1a3bf00c0e
RohitKModi777 <rohitkumar.modi777@gmail.com>
2026-04-03 10:45:28 +0530

    fixed teh error of google login

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,12 +5,20 @@ import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}>
+    {googleClientId ? (
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </GoogleOAuthProvider>
+    ) : (
       <AuthProvider>
         <App />
       </AuthProvider>
-    </GoogleOAuthProvider>
+    )}
   </React.StrictMode>
 );
